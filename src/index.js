9ce0fbc9892311613b1505dac93fd44da77ebaea
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2022-11-04 04:41:51 +0530

    API

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log("Server is up");
+});
