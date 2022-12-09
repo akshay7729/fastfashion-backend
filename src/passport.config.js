3d59ed68aa548182fb1af37ed892b18a4668bb41
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2022-12-09 16:15:19 +0530

    passport auth

--- src/passport.config.js
+++ src/passport.config.js
@@ -3,29 +3,7 @@ const bycrypt = require("bcryptjs");
 const { default: mongoose } = require("mongoose");
 const UserModal = require("../src/models/userModel");
 
-function initialize(passport, getUserByEmail, getUserById) {
-  // authenticate user
-  // const authenticateUser = async (email, password, done) => {
-  //   console.log("login progress till here", email, password);
-  //   const user = getUserByEmail(email);
-  //   if (user === null) {
-  //     return done(null, false, { message: "No user with that email found" });
-  //   }
-
-  //   try {
-  //     if (await bycrypt.compare(password, user.password)) {
-  //       return done(null, user);
-  //     } else {
-  //       return done(null, false, { message: "Password is incorrect" });
-  //     }
-  //   } catch (error) {
-  //     return done(error);
-  //   }
-  // };
-
-  // create local strategy
-  // passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
-
+function initialize(passport) {
   passport.use(
     new LocalStrategy(function (username, password, done) {
       (async () => {
@@ -34,13 +12,10 @@ function initialize(passport, getUserByEmail, getUserById) {
         const docs = await query;
 
         const getUser = docs.find((user) => user.userName === username);
-        console.log("getUser", getUser);
 
         if (!getUser) {
           return done(null, false);
         }
-
-        console.log("user exists");
 
         bycrypt.compare(password, getUser.password, (err, user) => {
           if (err) throw err;
@@ -53,28 +28,6 @@ function initialize(passport, getUserByEmail, getUserById) {
 
         return docs;
       })();
-
-      // UserModal.findOne({ username: username }, function (err, user) {
-      //   if (err) {
-      //     return done(err);
-      //   }
-      //   if (!user) {
-      //     return done(null, false);
-      //   }
-
-      //   bycrypt.compare(password, user.password, (err, result) => {
-      //     if (err) throw err;
-      //     if (result === true) {
-      //       return done(null, user);
-      //     } else {
-      //       return done(null, false);
-      //     }
-      //   });
-
-      //   // if (!user.verifyPassword(password)) {
-      //   //   return done(null, false);
-      //   // }
-      // });
     })
   );
 
